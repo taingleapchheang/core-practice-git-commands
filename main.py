4346a86153d9889345add3b55394e91096f57831
Taingleap Chheang <chheangtaingleap@gmail.com>
2022-03-24 11:11:25 -0700

    refactors always_returns_true() to return True

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pytest
 
 
-def always_returns_true():
-    return False
+def always_returns_true(value):
+    if bool(value) == True:
+        return True
 
 
 def test_always_returns_true():
-    assert always_returns_true()
+    #Arrange
+    value = 1
+
+    #Act
+    result = always_returns_true(value)
+
+    #Assert
+    assert bool(result) is True
